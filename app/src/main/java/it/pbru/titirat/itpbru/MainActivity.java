53daa1356fabf7e2c47d5b14496f1b58d5fe9d42
titirat <studiotech.titirat@gmail.com>
2016-06-07 15:59:49 +0700

    delete finish

--- app/src/main/java/it/pbru/titirat/itpbru/MainActivity.java
+++ app/src/main/java/it/pbru/titirat/itpbru/MainActivity.java
@@ -1,6 +1,7 @@
 package it.pbru.titirat.itpbru;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -19,7 +20,19 @@ public class MainActivity extends AppCompatActivity {
         //test Add new user
         //myManage.addNewUser("123", "name", "sur", "user", "pass");
 
+        //delete all sqlite
+        deleteAllSQLite();
+
     } //Main Method
+
+    private void deleteAllSQLite() {
+
+        SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name,
+                MODE_PRIVATE, null);
+        sqLiteDatabase.delete(MyManage.user_table, null, null);
+
+
+    } //deleteAllSQLite
 
     public void clickSingUpMain(View view) {
       startActivity(new Intent(MainActivity.this,SignUpActivity.class));
